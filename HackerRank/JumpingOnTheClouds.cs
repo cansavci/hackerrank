fa36c0ed13eff17a52a92fe36c76b02a4ecd13b7
cansavci <mcansavci@gmail.com>
2020-02-19 21:30:21 +0300

    Warm up Challenges completed

---
+++ HackerRank/JumpingOnTheClouds.cs
@@ -0,0 +1,28 @@
+﻿namespace HackerRank
+{
+    class JumpingOnTheClouds
+    {
+        int solution(int[] c)
+        {
+            int jumpsCount = 0;
+            int sizeOfC = c.Length;
+            for (int i = 0; i < sizeOfC; i++)
+            {
+                if (i == sizeOfC - 1)
+                {
+                    jumpsCount++;
+                }
+                else if (c[i + 2] == 0)
+                {
+                    i++;
+                    jumpsCount++;
+                }
+                else
+                {
+                    jumpsCount++;
+                }
+            }
+            return jumpsCount;
+        }
+    }
+}
---
+++ HackerRank/RepeatedString.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace HackerRank
+{
+    class RepeatedString
+    {
+        long solution(string s, long n)
+        {
+            int lengthOfS = s.Length;
+
+            long total = 0;
+            foreach (char c in s)
+            {
+                if (c == 'a') total++;
+            }
+
+            long numberForSeperation = n % lengthOfS;
+            long times = n / lengthOfS;
+            total = total * times;
+
+            string remaining = s.Substring(0, Convert.ToInt32(numberForSeperation));
+            foreach (char c in remaining)
+            {
+                if (c == 'a') total++;
+            }
+
+            return total;
+        }
+    }
+}
