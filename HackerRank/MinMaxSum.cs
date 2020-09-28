ee9ecfa770ea5063186f50147e96081b50e5a862
Can <can@Can-MacBook-Pro.local>
2020-09-28 22:27:35 +0300

    Min Max

---
+++ HackerRank/MinMaxSum.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace HackerRank
+{
+    class MinMaxSum
+    {
+        void solution(int[] arr)
+        {
+            long maxValSum = 0;
+            long minValSum = 0;
+            foreach (var elem in arr)
+            {
+                if (elem == arr.Min())
+                {
+                    minValSum = minValSum + elem;
+                }
+                else if (elem == arr.Max())
+                {
+                    maxValSum = maxValSum + elem;
+                }
+                else
+                {
+                    minValSum = minValSum + elem;
+                    maxValSum = maxValSum + elem;
+                }
+            }
+            Console.WriteLine(minValSum + " " + maxValSum);
+        }
+    }
+}
