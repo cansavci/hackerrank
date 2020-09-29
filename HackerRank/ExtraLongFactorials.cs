fa6f4849689fec36b05c70a66c7a52b122d9d95e
cansavci <mcansavci@gmail.com>
2020-09-29 22:26:02 +0300

    One more to write

---
+++ HackerRank/ExtraLongFactorials.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Numerics;
+
+namespace HackerRank
+{
+    public class ExtraLongFactorials
+    {
+        void solution(int n)
+        {
+            var bi = new BigInteger(1);
+            for (var i = 1; i <= n; i++)
+            {
+                bi *= i;
+            }
+            Console.Write(bi.ToString());
+        }
+    }
+}
